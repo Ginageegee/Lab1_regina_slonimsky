5adc89cd0a653cbecbc4bb5d0bee57e7f5791402
ginaslonimsky <regina.slonimsky@hotmail.com>
2026-02-28 00:06:15 -0500

    Initial Commit

---
+++ Lab1_regina_slonimsky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_regina_slonimsky
+//
+//  Created by Gina Slonimsky  on 2026-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_regina_slonimsky/Lab1_regina_slonimskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_regina_slonimskyApp.swift
+//  Lab1_regina_slonimsky
+//
+//  Created by Gina Slonimsky  on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_regina_slonimskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
